c5171644381e7084a04606d76b39976aacbd6b87
John Ensley <johnensley17@gmail.com>
2022-11-28 09:47:27 -0700

    push changes

--- src/pwlreg/pwlreg.py
+++ src/pwlreg/pwlreg.py
@@ -1,11 +1,9 @@
 import itertools
 import numbers
-import warnings
 
 import numpy as np
 from scipy import linalg
 from scipy.optimize import differential_evolution
-import scipy.sparse as sparse
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils import check_random_state, check_X_y, check_array
 from sklearn.utils.validation import check_is_fitted
@@ -31,48 +29,7 @@ def _check_sample_weight(weights, X, dtype):
     return weights
 
 
-def _rescale_data(X, y, sample_weight):
-    n_samples = X.shape[0]
-    sample_weight = np.asarray(sample_weight)
-    sample_weight_sqrt = np.sqrt(sample_weight)
-    sw_matrix = sparse.dia_matrix((sample_weight_sqrt, 0), shape=(n_samples, n_samples))
-    X = sw_matrix @ X
-    y = sw_matrix @ y
-    return X, y, sample_weight_sqrt
-
-
-# def _assemble_regression_matrix(X, breaks):
-#     breaks = np.sort(breaks)
-#     A = np.column_stack([np.clip(X - b, 0., None) for b in breaks[:-1]])
-#     A = np.insert(A, 0, 1, axis=1)
-#     return A
-
-
-def _assemble_regression_matrix(X, breaks, degree=1):
-    breaks = np.sort(breaks)
-    if isinstance(degree, numbers.Number):
-        degree = [degree] * (len(breaks) - 1)
-
-    Acols = [np.ones_like(X)]
-    for b, d in zip(breaks[:-1], degree):
-        if d == 0:
-            idx = np.argwhere(X > b)
-            for j in range(1, len(Acols)):
-                Acols[j][idx] = 0.
-            Acols.append(np.where(X > b, 1., 0.))
-        else:
-            for k in range(1, d + 1):
-                Acols.append(np.clip((X - b) ** k, 0., None))
-
-    A = np.column_stack(Acols)
-    return A
-
-
-def _assemble_regression_equation(X, breaks, degree=1, continuity="c0"):
-    breaks = np.sort(breaks)
-    if isinstance(degree, numbers.Number):
-        degree = [degree] * (len(breaks) - 1)
-
+def _assemble_regression_matrix(X, breaks, degree):
     Acols = []
     bins = np.digitize(X, breaks).clip(None, len(breaks) - 1)
     for i, d in enumerate(degree):
@@ -80,11 +37,11 @@ def _assemble_regression_equation(X, breaks, degree=1, continuity="c0"):
             Acols.append(np.where(bins == i+1, X ** k, 0.))
 
     A = np.column_stack(Acols)
-    _, m = A.shape
+    return A
 
-    if continuity is None:
-        return A, A, m, 0
 
+def _assemble_continuity_constraints(breaks, degree):
+    m = len(degree) + sum(degree)
     Crows = []
     i = 0
     for b, (d0, d1) in zip(breaks[1:-1], itertools.pairwise(degree)):
@@ -95,14 +52,7 @@ def _assemble_regression_equation(X, breaks, degree=1, continuity="c0"):
         i += d0 + 1
 
     C = np.row_stack(Crows)
-    o, _ = C.shape
-
-    K = np.zeros((m + o, m + o))
-    K[:m, :m] = np.dot(A.T, A)
-    K[:m, m:] = C.T
-    K[m:, :m] = C
-
-    return K, A, m, o
+    return C
 
 
 def _augment_breaks(breaks, break_0, break_n):
@@ -112,15 +62,10 @@ def _augment_breaks(breaks, break_0, break_n):
     return b_
 
 
-def _fit_opt(breaks, X, y, break_0, break_n, degree, continuity="c0"):
+def _fit_opt(breaks, X, y, break_0, break_n, degree, continuity="c0", weights=None):
     b_ = _augment_breaks(breaks, break_0, break_n)
-    K, A, m, o = _assemble_regression_equation(X, b_, degree, continuity)
     try:
-        Z = np.zeros(m + o)
-        Z[:m] = np.dot(A.T, y)
-        beta = linalg.solve(K, Z)
-        e = np.dot(A, beta[:m]) - y
-        ssr = np.dot(e, e)
+        _, ssr = _lstsq_constrained(X, y, b_, degree, continuity, weights)
     except linalg.LinAlgError as e:
         print(e)
         ssr = np.inf
@@ -143,17 +88,52 @@ def _safe_lstsq(A, y):
     return coef_, ssr, rank_, singular_
 
 
+def _lstsq_constrained(X, y, breaks, degree=1, continuity="c0", weights=None):
+    breaks = np.sort(breaks)
+    if isinstance(degree, numbers.Number):
+        degree = [degree] * (len(breaks) - 1)
+
+    A = _assemble_regression_matrix(X, breaks, degree)
+    n, p = A.shape
+
+    if weights is None:
+        weights = np.ones(shape=n)
+
+    Aw = A * weights.reshape(-1, 1)
+    yw = y * weights
+
+    if continuity is not None and len(breaks) > 2:
+        C = _assemble_continuity_constraints(breaks, degree)
+        r, _ = C.shape
+
+        K = np.zeros((p + r, p + r))
+        K[:p, :p] = np.dot(Aw.T, Aw)
+        K[:p, p:] = C.T
+        K[p:, :p] = C
+
+        Z = np.zeros(p + r)
+        Z[:p] = np.dot(Aw.T, yw)
+
+        beta = linalg.solve(K, Z)
+        coef = beta[:p]
+
+        e = np.dot(A, coef) - y
+        ssr = np.dot(e, e)
+    else:
+        coef, ssr, _, _ = _safe_lstsq(Aw, yw)
+
+    return coef, ssr
+
+
 def _auto_piecewise_regression(
         X,
         y,
         n_segments,
         degree,
         continuity="c0",
-        sample_weight=None,
         solver="auto",
         max_iter=None,
         tol=1e-4,
-        verbose=0,
         random_state=False,
         return_n_iter=False,
 ):
@@ -236,77 +216,79 @@ def _solve_minimize(
     return 0, 0
 
 
-class PiecewiseRegressorMixin(RegressorMixin):
-    def _decision_function(self, X):
+class _BasePiecewiseRegressor(RegressorMixin):
+    def __init__(self, degree, continuity):
+        self.degree = degree
+        self.continuity = continuity
+
+    def _assemble_regression_matrix(self, X, breaks):
+        return _assemble_regression_matrix(X, breaks, self.degree)
+
+    # noinspection PyTypeChecker
+    def _decision_function(self, X, breaks):
         X = check_array(X, accept_sparse=True, ensure_2d=False)
-        check_is_fitted(self, "breakpoints_")
-
-        # noinspection PyTypeChecker
-        _, A, _, _ = _assemble_regression_equation(
-            X,
-            self.breakpoints_,
-            self.degree,
-            self.continuity,
-        )
-
+        check_is_fitted(self)
+        A = self._assemble_regression_matrix(X, breaks)
         return np.dot(A, self.coef_)
 
-    def fit_with_breaks(self, X, y, breaks, degree, continuity):
+    def fit_with_breaks(self, X, y, breaks, weights=None):
         breaks = breaks if isinstance(breaks, np.ndarray) else np.array(breaks)
-        K, A, m, o = _assemble_regression_equation(X, breaks, degree, continuity)
-        Z = np.zeros(m + o)
-        Z[:m] = np.dot(A.T, y)
-        beta = linalg.solve(K, Z)
-        self.coef_ = beta[:m]
-        e = np.dot(A, self.coef_) - y
-        self.ssr_ = np.dot(e, e)
-        self.n_params_ = m
+
+        if isinstance(self.degree, numbers.Number):
+            self.degree = [self.degree] * (len(breaks) - 1)
+
+        if len(self.degree) != len(breaks) - 1:
+            msg = "With {} breakpoints, the model will fit {} segment(s). " \
+                  "However, {} degree values were supplied. " \
+                  "The number of degree values must match the number of segments."
+            raise ValueError(msg.format(len(breaks), len(breaks) - 1, len(self.degree)))
+
+        _valid_continuities = ("c0",)
+        if self.continuity and self.continuity not in _valid_continuities:
+            raise ValueError(
+                "Continuity must be one of: {}. Got {}".format(_valid_continuities, self.continuity)
+            )
+
+        self.coef_, self.ssr_ = _lstsq_constrained(X, y, breaks, self.degree, self.continuity, weights)
+        self.n_params_ = len(self.coef_)
+        self.is_fitted_ = True
         return self
 
 
-class PiecewiseLinearRegression(BaseEstimator, PiecewiseRegressorMixin):
+class PiecewiseLinearRegression(BaseEstimator, _BasePiecewiseRegressor):
     def __init__(self, *, breakpoints=None, degree=1, continuity="c0"):
         self.breakpoints = breakpoints
-        self.degree = degree
-        self.continuity = continuity
+        super().__init__(degree, continuity)
 
     def fit(self, X, y, *, weights=None):
         X, y = check_X_y(X, y, accept_sparse=True, ensure_2d=False, y_numeric=True)
         weights = _check_sample_weight(weights, X, dtype=X.dtype)
-        X, y, sample_weight_sqrt = _rescale_data(X, y, weights)
-
-        break_0, break_n = np.min(X), np.max(X)
 
         if self.breakpoints is None:
-            self.breakpoints_ = np.array([break_0, break_n])
-        else:
-            self.breakpoints_ = self.breakpoints
+            self.breakpoints = np.array([np.min(X), np.max(X)])
 
-        self.fit_with_breaks(X, y, self.breakpoints_, self.degree, self.continuity)
-        self.is_fitted_ = True
+        self.fit_with_breaks(X, y, self.breakpoints, weights)
         return self
 
     def predict(self, X):
-        return self._decision_function(X)
+        return self._decision_function(X, self.breakpoints)
 
 
-class AutoPiecewiseRegression(BaseEstimator, PiecewiseRegressorMixin):
+class AutoPiecewiseRegression(BaseEstimator, _BasePiecewiseRegressor):
     def __init__(self, n_segments, *, degree=1, continuity="c0", solver="auto", random_state=None):
         self.n_segments = n_segments
-        self.degree = degree
-        self.continuity = continuity
         self.solver = solver
         self.random_state = random_state
+        super().__init__(degree, continuity)
 
     def fit(self, X, y, weights=None):
         random_state_ = check_random_state(self.random_state)
         X, y = check_X_y(X, y, accept_sparse=True, ensure_2d=False, y_numeric=True)
         weights = _check_sample_weight(weights, X, dtype=X.dtype)
-        X, y, sample_weight_sqrt = _rescale_data(X, y, weights)
 
         if self.n_segments == 1:
-            self.fit_with_breaks(X, y, np.array([np.min(X), np.max(X)]), self.degree, self.continuity)
-            return self
+            self.breakpoints_ = np.array([np.min(X), np.max(X)])
+            return self.fit_with_breaks(X, y, self.breakpoints_, weights)
 
         self.breakpoints_, self.n_iter = _auto_piecewise_regression(
             X,
@@ -314,15 +296,12 @@ class AutoPiecewiseRegression(BaseEstimator, PiecewiseRegressorMixin):
             self.n_segments,
             self.degree,
             self.continuity,
-            sample_weight=weights,
             solver=self.solver,
             random_state=random_state_,
             return_n_iter=True,
         )
 
-        self.fit_with_breaks(X, y, self.breakpoints_, self.degree, self.continuity)
-        self.is_fitted_ = True
-        return self
+        return self.fit_with_breaks(X, y, self.breakpoints_)
 
     def predict(self, X):
-        return self._decision_function(X)
+        return self._decision_function(X, self.breakpoints_)
--- tests/test_pwlreg.py
+++ tests/test_pwlreg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.testing as nptest
+import pytest
 from sklearn.linear_model import LinearRegression
 
 import pwlreg as pw
@@ -8,8 +9,7 @@ import pwlreg as pw
 class TestPWLReg:
     x_small = np.array([0.0, 1.0, 1.5, 2.0])
     y_small = np.array([0.0, 1.0, 1.1, 1.5])
-    x_sin = np.linspace(0, 10, num=100)
-    y_sin = np.sin(x_sin * np.pi / 2)
+
     y = np.array([0.00000000e+00, 9.69801700e-03, 2.94350340e-02,
                   4.39052750e-02, 5.45343950e-02, 6.74104940e-02,
                   8.34831790e-02, 1.02580042e-01, 1.22767939e-01,
@@ -51,27 +51,97 @@ class TestPWLReg:
                   1.22120350e-01, 1.36931660e-01, 1.50958760e-01,
                   1.65299640e-01, 1.79942720e-01])
 
-    def test_pwlr(self):
-        m = pw.AutoPiecewiseRegression(n_segments=2)
-        m.fit(self.x_small, self.y_small)
+    x_wt = np.array([65.43888437, 25.47311425, 61.85749766, 17.5333924, 21.37950291,
+                     7.91211261, 16.19834165, 21.34177323, 64.59330943, 17.66453689,
+                     29.54741018, 40.86134423, 95.49950279, 95.50400314, 89.27108334,
+                     88.77585348, 91.28000404, 74.35087072, 72.67818409, 95.72369409,
+                     68.98457571, 89.561734, 89.15085463, 87.16359338, 68.98453132,
+                     99.26638603, 81.48540368, 84.57563571, 67.10336549, 75.29181448,
+                     95.33018443, 81.03484559, 91.2820268, 97.42742615, 72.06464765,
+                     99.74455457, 73.01694883, 98.02372578, 69.86714082, 82.4509536,
+                     94.3566436, 76.27472468, 95.68001987, 99.22174695, 94.7765507,
+                     81.81282881, 79.22677818, 82.92809142, 70.04199105, 74.48054247,
+                     84.70668581, 91.19664449, 81.81405603, 77.0815254, 98.26286691,
+                     91.7841166, 82.57759918, 82.09010403, 91.62914751, 95.22819597])
+    y_wt = np.array([8.97880375, 10.848107, 10.49808261, 9.91555836, 10.20249333,
+                     9.83619422, 10.83705971, 9.28756034, 8.82585013, 10.47526795,
+                     11.73739481, 9.86335634, 33.23462012, 23.78659682, 27.89500429,
+                     24.9981975, 24.56649716, 15.64591645, 11.28936398, 24.8824468,
+                     8.95420215, 13.4774194, 25.04721503, 19.4818676, 12.29725968,
+                     26.89833196, 14.78996983, 29.3125192, 11.45011255, 14.53061505,
+                     24.28185342, 22.14938715, 20.64677086, 33.69656426, 13.68423099,
+                     18.55806064, 14.42253021, 31.82904077, 12.21504628, 20.92386072,
+                     30.69449996, 13.46260248, 36.29032943, 27.05361143, 28.19597908,
+                     12.31542127, 18.9774047, 20.41061017, 10.67105297, 15.1893247,
+                     20.70096165, 26.7345677, 19.71340063, 13.64807196, 25.94367206,
+                     14.37849188, 17.86041382, 15.86200352, 27.33210288, 31.14143882])
+    wts = np.array([1., 1., 1., 1., 1.,
+                    1., 1., 1., 1., 1.,
+                    1., 1., 5.27492542, 5.27560047, 4.3406625,
+                    4.26637802, 4.64200061, 2.10263061, 1.85172761, 5.30855411,
+                    1.29768636, 4.3842601, 4.32262819, 4.02453901, 1.2976797,
+                    5.8399579, 3.17281055, 3.63634536, 1.01550482, 2.24377217,
+                    5.24952767, 3.10522684, 4.64230402, 5.56411392, 1.75969715,
+                    5.91168319, 1.90254233, 5.65355887, 1.43007112, 3.31764304,
+                    5.10349654, 2.3912087, 5.30200298, 5.83326204, 5.1664826,
+                    3.22192432, 2.83401673, 3.38921371, 1.45629866, 2.12208137,
+                    3.65600287, 4.62949667, 3.22210841, 2.51222881, 5.68943004,
+                    4.71761749, 3.33663988, 3.2635156, 4.69437213, 5.2342294])
 
-    def test_matrix_assembly(self):
+    def test_matrix_assembly_deg1(self):
         x0 = self.x_small.copy()
-        A = pw.pwlreg._assemble_regression_matrix(x0, breaks=x0)
+        breaks = [x0.min(), 1.25, x0.max()]
+        A = pw.pwlreg._assemble_regression_matrix(self.x_small, breaks, degree=[1, 1])
         Atruth = np.array(
             [
-                [1.,            0.,            0.,            0.],
-                [1., x0[1] - x0[0],            0.,            0.],
-                [1., x0[2] - x0[0], x0[2] - x0[1],            0.],
-                [1., x0[3] - x0[0], x0[3] - x0[1], x0[3] - x0[2]],
+                [1., x0[0], 0., 0.],
+                [1., x0[1], 0., 0.],
+                [0., 0., 1., x0[2]],
+                [0., 0., 1., x0[3]],
             ]
         )
         assert np.allclose(A, Atruth)
 
+    def test_matrix_assembly_deg2(self):
+        x0 = self.x_small.copy()
+        breaks = [x0.min(), 1.25, x0.max()]
+        A = pw.pwlreg._assemble_regression_matrix(self.x_small, breaks, degree=[2, 2])
+        Atruth = np.array(
+            [
+                [1., x0[0], x0[0] ** 2, 0., 0., 0.],
+                [1., x0[1], x0[1] ** 2, 0., 0., 0.],
+                [0., 0., 0., 1., x0[2], x0[2] ** 2],
+                [0., 0., 0., 1., x0[3], x0[3] ** 2],
+            ]
+        )
+        assert np.allclose(A, Atruth)
+
+    def test_continuity_constraints_deg1(self):
+        breaks = [np.min(self.x_small), 1.25, 1.75, np.max(self.x_small)]
+        C = pw.pwlreg._assemble_continuity_constraints(breaks, degree=[1, 1, 1])
+        Ctruth = np.array(
+            [
+                [1., breaks[1], -1., -breaks[1], 0., 0.],
+                [0., 0., 1., breaks[2], -1., -breaks[2]],
+            ]
+        )
+        assert np.allclose(C, Ctruth)
+
+    def test_continuity_constraints_deg2(self):
+        b = [np.min(self.x_small), 1.25, 1.75, np.max(self.x_small)]
+        C = pw.pwlreg._assemble_continuity_constraints(b, degree=[2, 2, 2])
+        Ctruth = np.array(
+            [
+                [1., b[1], b[1] ** 2, -1., -b[1], -b[1] ** 2, 0., 0., 0.],
+                [0., 0., 0., 1., b[2], b[2] ** 2, -1., -b[2], -b[2] ** 2],
+            ]
+        )
+        assert np.allclose(C, Ctruth)
+
     def test_fit(self):
-        m = pw.AutoPiecewiseRegression(n_segments=4)
-        m.fit(self.x_small, self.y_small)
-        assert np.isclose(m.ssr_, 0.)
+        m = pw.AutoPiecewiseRegression(n_segments=3)
+        m.fit(self.x, self.y)
+        assert 2.438e-4 == pytest.approx(m.ssr_, rel=1e-2)
 
     def test_fit_rng(self):
         seed = 1234
@@ -81,8 +151,8 @@ class TestPWLReg:
         coefs2 = m.fit(self.x_small, self.y_small).coef_
         nptest.assert_equal(coefs1, coefs2)
 
-    def test_predict_nobreaks(self):
-        m = pw.PiecewiseLinearRegression(breakpoints=[np.min(self.x_small), np.max(self.x_small)])
+    def test_predict_nobreaks_equals_sklearn_regression(self):
+        m = pw.PiecewiseLinearRegression()
         m.fit(self.x_small, self.y_small)
         y_pred = m.predict(self.x_small)
 
@@ -92,3 +162,37 @@ class TestPWLReg:
         y2_pred = m2.predict(X)
 
         assert np.allclose(y_pred, y2_pred)
+
+    def test_pw_degree_shortform(self):
+        m = pw.PiecewiseLinearRegression(
+            breakpoints=[np.min(self.x), 0.04, np.max(self.x)],
+            degree=1,
+        )
+        m.fit(self.x, self.y)
+        assert m.degree == [1, 1]
+
+    def test_pw_wrong_number_of_degrees(self):
+        m = pw.PiecewiseLinearRegression(degree=[1, 2, 3])
+        with pytest.raises(
+                ValueError,
+                match="The number of degree values must match the number of segments",
+        ):
+            m.fit(self.x, self.y)
+
+    def test_pw_wrong_continuity(self):
+        m = pw.PiecewiseLinearRegression(continuity="c1000")
+        with pytest.raises(ValueError, match="Continuity must be one of"):
+            m.fit(self.x, self.y)
+
+    def test_pw_weights(self):
+        m = pw.PiecewiseLinearRegression(
+            breakpoints=[np.min(self.x_wt), 65, np.max(self.x_wt)],
+            degree=[0, 1],
+        )
+        m.fit(self.x_wt, self.y_wt, weights=1 / self.wts)
+        assert 862.5051 == pytest.approx(m.ssr_, rel=1e-2)
+
+    def test_autopw_wrong_solver(self):
+        m = pw.AutoPiecewiseRegression(n_segments=3, solver="blarg")
+        with pytest.raises(ValueError, match="Valid solvers are"):
+            m.fit(self.x, self.y)
